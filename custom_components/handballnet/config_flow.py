a5273ec261c8dcb16101819e2bfa3a5644454813
Miguel <miguel.gebhardt@bechtle.com>
2025-05-23 22:45:02 +0200

    feat: changed logic

--- custom_components/handballnet/config_flow.py
+++ custom_components/handballnet/config_flow.py
@@ -1,20 +1,24 @@
-from homeassistant import config_entries
 import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.core import callback
 from .const import DOMAIN, CONF_TEAM_ID
 
 class HandballNetConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        errors = {}
         if user_input is not None:
-            return self.async_create_entry(
-                title=f"Team {user_input[CONF_TEAM_ID]}", data=user_input
-            )
+            team_id = user_input.get(CONF_TEAM_ID)
+            if not team_id or not team_id.isdigit():
+                errors[CONF_TEAM_ID] = "invalid_team_id"
+            else:
+                return self.async_create_entry(title=f"Team {team_id}", data=user_input)
+
+        data_schema = vol.Schema({
+            vol.Required(CONF_TEAM_ID): str,
+        })
 
         return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({
-                vol.Required(CONF_TEAM_ID): str
-            }),
-            errors={}
+            step_id="user", data_schema=data_schema, errors=errors
         )
