1639f71fc9b8049b99db5f6d9d8e9976c3435e4f
Miguel <miguel.gebhardt@bechtle.com>
2025-05-26 21:21:06 +0200

    fix: :bug: fixed config not showing

--- custom_components/handballnet/options_flow.py
+++ custom_components/handballnet/options_flow.py
@@ -12,17 +12,27 @@ class HandballNetOptionsFlowHandler(config_entries.OptionsFlow):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
 
-        options = self.config_entry.options
-        current_interval = options.get(CONF_UPDATE_INTERVAL, DEFAULT_UPDATE_INTERVAL)
-
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema({
-                vol.Optional(CONF_UPDATE_INTERVAL, default=DEFAULT_UPDATE_INTERVAL): int,
-                vol.Optional(CONF_UPDATE_INTERVAL_LIVE, default=DEFAULT_UPDATE_INTERVAL_LIVE): int,
+                vol.Optional(
+                    CONF_UPDATE_INTERVAL,
+                    default=self.config_entry.options.get(
+                        CONF_UPDATE_INTERVAL,
+                        self.config_entry.data.get(CONF_UPDATE_INTERVAL, DEFAULT_UPDATE_INTERVAL)
+                    )
+                ): int,
+                vol.Optional(
+                    CONF_UPDATE_INTERVAL_LIVE,
+                    default=self.config_entry.options.get(
+                        CONF_UPDATE_INTERVAL_LIVE,
+                        self.config_entry.data.get(CONF_UPDATE_INTERVAL_LIVE, DEFAULT_UPDATE_INTERVAL_LIVE)
+                    )
+                ): int,
             }),
             errors=errors
         )
+
 
 @callback
 def async_get_options_flow(config_entry):
